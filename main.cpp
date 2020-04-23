cc385fc015bf979f24c856ba7195c806987ccd70
Avinoam <avinoam@hershler.com>
2020-04-23 21:48:31 +0300

    basic test for switchToThread

--- main.cpp
+++ main.cpp
@@ -2,17 +2,27 @@
 #include <zconf.h>
 #include "uthreads.cpp"
 
+void f();
+void g();
+
+static Thread thread1(1, 1, f);
+static Thread thread2(2, 1, g);
+std::shared_ptr<Thread> pThread1(&thread1);
+std::shared_ptr<Thread> pThread2(&thread2);
+
+Dispatcher dispatcher;
+
 void f(void)
 {
     int i = 0;
     while(1){
         ++i;
         printf("in f (%d)\n",i);
-        if (i > 100)
+        if (i % 10 == 0)
         {
-
-        }
-        usleep(1000000);
+			dispatcher.switchToThread(pThread1, pThread2);
+		}
+        usleep(10000);
     }
 }
 
@@ -22,17 +32,18 @@ void g(void)
     while(1){
         ++i;
         printf("in g (%d)\n",i);
-        usleep(1000000);
+		if (i % 10 == 0)
+		{
+			dispatcher.switchToThread(pThread2, pThread1);
+		}
+        usleep(10000);
     }
 }
 
 
 int main()
 {
-    static Thread thread1(1, 1, f);
-    static Thread thread2(1, 1, g);
-    Dispatcher dispatcher;
-    dispatcher.switchToThread(std::shared_ptr<Thread>(&thread1), std::shared_ptr<Thread>(&thread2));
-
-    return 0;
+	f();
+//	dispatcher.switchToThread(pThread1, pThread2);
+	return 0;
 }
